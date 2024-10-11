11cb09e3b66f699cf7a3e3d592a39a803b0c71c8
Danie <danievdm85@gmail.com>
2024-10-11 18:30:46 +0200

    Question2.1

---
+++ CheckUserSettingEnabled.sql
@@ -0,0 +1,16 @@
+﻿CREATE FUNCTION [dbo].[CheckUserSettingEnabled]
+(
+	@settings char(8),
+	@setting int 
+)
+RETURNS BIT
+AS
+BEGIN
+	DECLARE @result BIT;
+	IF SUBSTRING(@settings, @setting, 1) = '1'
+		SET @result = 1;
+    ELSE
+        SET @result = 0;
+
+	RETURN @result;
+END
